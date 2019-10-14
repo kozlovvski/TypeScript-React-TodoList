3a19dbbd24d96f77c791894c171fbef48c347dc9
Michał Kozłowski <mkozlowski@animativ.pl>
2019-10-14 12:28:14 +0200

    add basic logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Fragment, useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+type FormElem = React.FormEvent<HTMLFormElement>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
+export default function App(): JSX.Element {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (givenIndex: number): void => {
+    const newTodos = todos.filter((todo: ITodo, index: number) => index !== givenIndex)
+    setTodos(newTodos)
+  }
+
+  return (
+    <>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      <section>
+        {todos.map((todo: ITodo, index: number) => (
+          <Fragment key={index}>
+            <div>{todo.text}</div>
+            <button onClick={() => completeTodo(index)}>
+              {todo.complete ? "Incomplete" : "Complete"}{" "}
+            </button>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </Fragment>
+        ))}
+      </section>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
